eedc6cc9543048720fc04fb268b9583d7091c9df
Mathew Brown <matcoffe3@gmail.com>
2017-11-10 11:27:20 -0500

    updated yesterdays games

--- runstuff.sh
+++ runstuff.sh
@@ -2,7 +2,7 @@
 set -x; exec 2>/tmp/mycommand.log
 
 cd /projects/habs
-python /projects/habs/load_data.py
+python3 /projects/habs/load_data.py
 
 git add .
 git commit -am'updated yesterdays games'
